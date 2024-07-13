16eab511a39c216a753c028b3165e8ec4f4c97a1
kamelEddinee <kameleddineayache@gmail.com>
2024-07-13 12:27:15 +0100

    hide the token

--- src/Api/axios.ts
+++ src/Api/axios.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export const MoviesApiFetch = axios.create({
-  baseURL: "https://0kadddxyh3.execute-api.us-east-1.amazonaws.com",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   headers: {
     Accept: "application/json",
     "Accept-Language": "en",
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJvcGVuSnd0MSIsIm5hbWUiOiJPcGVuSldUWzFdIn0.n8x8GHYe8RQYKkAoMVMlw9-FMZ57bs0HrwxBeJn3hQM`,
+    Authorization: process.env.NEXT_PUBLIC_TOKEN,
   },
 });
